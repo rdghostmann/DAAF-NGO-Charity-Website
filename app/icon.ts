466a5c0e5b715585b63473771b3dc03da0f36f70
unknown <randalchukzwilson@gmail.com>
2025-05-23 13:39:39 +0100

    Added favicon

--- app/icon.ts
+++ app/icon.ts
@@ -1,38 +1,35 @@
 import { ImageResponse } from 'next/og'
- 
+
 // Image metadata
 export const size = {
   width: 32,
   height: 32,
 }
 export const contentType = 'image/png'
- 
+
 // Image generation
-export default function Icon() {
+export default function Icon(): ImageResponse {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
           fontSize: 24,
           background: 'green',
-          width: '100%',
-          height: '100%',
+          width: size.width,
+          height: size.height,
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
           color: 'white',
-          borderRadius: '100%'
+          borderRadius: '100%',
         }}
       >
         F
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported icons size metadata
-      // config to also set the ImageResponse's width and height.
-      ...size,
+      width: size.width,
+      height: size.height,
     }
   )
 }
